5822aa9ce4ef5c9667005ad9b6fe6ed750aab8d7
shreesofthub <sridhar.b254@gmail.com>
2022-04-22 07:15:34 +0530

    Initiall Commit

---
+++ webapp/controller/root_View.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("hckt.sapYardsapYard_01rn.controller.root_View", {
+
+	});
+});
